4baa3b33a3fec1c271f5f39eab8a77784e886a8d
Lucas Dib <49247275+LucasDibz@users.noreply.github.com>
2019-10-06 16:07:53 -0300

    Update SpeechToTextServlet.java

--- src/main/java/br/com/fiap/watson_api/SpeechToTextServlet.java
+++ src/main/java/br/com/fiap/watson_api/SpeechToTextServlet.java
@@ -43,8 +43,8 @@ public class SpeechToTextServlet extends HttpServlet {
 
 		RecognizeOptions recognizeOptions = new RecognizeOptions.Builder().audio(tempFile)
 				.contentType(HttpMediaType.AUDIO_WAV).model("pt-BR_BroadbandModel")
-//				.languageCustomizationId("<language_customization_id>")
-//				.acousticCustomizationId("<acousticCustomizationId>")
+				.languageCustomizationId("<language_customization_id>")
+				.acousticCustomizationId("<acousticCustomizationId>")
 				.build();
 
 		SpeechRecognitionResults transcript = service.recognize(recognizeOptions).execute().getResult();
