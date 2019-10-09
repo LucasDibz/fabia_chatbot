7b6907f8d63b57eac5b1896666da844318de62ce
Lucas Dib <49247275+LucasDibz@users.noreply.github.com>
2019-10-09 20:58:28 -0300

    Update Conexao.java

--- src/main/java/br/com/fiap/conexao/Conexao.java
+++ src/main/java/br/com/fiap/conexao/Conexao.java
@@ -6,6 +6,7 @@ import java.sql.DriverManager;
 public class Conexao {
 
 	public static Connection getConnection() throws Exception {
+		Class.forName("oracle.jdbc.driver.OracleDriver");
 		return DriverManager.getConnection("jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL", "<username>",
 				"<password>");
 	}
